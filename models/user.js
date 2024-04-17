5e3e04972410f7df1075b1c3d8e055e50a351e05
Samuel Spink <dr.greenjet@gmail.com>
2024-04-17 20:55:23 +0200

    feat: user model for authentication

---
+++ models/user.js
@@ -0,0 +1,49 @@
+const express = require("express");
+/* MongoDB Connection Package  */
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+/* Define the user schema */
+const userSchema = new Schema({
+    userId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+/* Create a virtual property for the full name */
+userSchema.virtual('fullName').get(function() {
+  return `${this.firstName} ${this.lastName}`;
+});
+
+/* Create the user model */
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
