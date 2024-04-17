5aeff6b83ab9f7b5130ada46db45ce0ce729ec1a
Samuel Spink <dr.greenjet@gmail.com>
2024-04-17 18:27:30 +0200

    feat: swagger api documentation auto generator script

---
+++ docs/generate.js
@@ -0,0 +1,18 @@
+/* Swagger Auto Generation Package Import */
+const swaggerAutogen = require("swagger-autogen")();
+
+/* API Document Informations */
+const doc = {
+    info: {
+      title: "Bibliothek",
+      description: "Description"
+    },
+    host: "localhost:3000"
+};
+/* Documentation Output File */
+const outputFile = "./swagger.json";
+/* Array of all route files */
+const routes = ["../main.js", "../routes"];
+
+/* Generation of the documentation */
+swaggerAutogen(outputFile, routes, doc);
