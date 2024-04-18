c11321c233fcf78e37b8f8a4c72ea8e31c87d9c9
Samuel Spink <dr.greenjet@gmail.com>
2024-04-18 11:20:42 +0200

    fix: bug fix, added JWT_SECRET & JWT_OPTIONS constants

--- modules/authentication.js
+++ modules/authentication.js
@@ -1,6 +1,10 @@
 /* Token Generation & Verification Package */
 const jwt = require("jsonwebtoken");
 
+/* Constants */
+const JWT_SECRET = "iSxILagNCfvu2jGQcrt7glDSZgC9pT4KNNxevrndjoQRJRxHAZDwc6xdpSHUyfA6";
+const JWT_OPTIONS = {expiresIn: "1h"};
+
 /* Token Generation & Authentication Functions */
 const generateAccessToken = (payload, callback) => {
     return jwt.sign(payload, JWT_SECRET, JWT_OPTIONS, callback);
