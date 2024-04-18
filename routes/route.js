d954fbdfc37c82ecb02772128d4ba3322a1fdd87
Samuel Spink <dr.greenjet@gmail.com>
2024-04-18 14:28:01 +0200

    feat: bodyParser is now only on the /api endpoint

--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+/* Express Router */
 const router = express.Router();
 router.get("/", (req, res) => res.sendStatus(200));
 
 /* Middleware(s) */
-
+router.use(bodyParser.json());
 
 /* API Endpoints */
 const auth = require("./auth");
